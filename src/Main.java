84d06291c9088ee8be5c271e24e5eda56cc63fe5
alexnezumi <alexsilva.viana2006@gmail.com>
2025-09-02 08:38:46 -0300

    primeiro exercicio

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    // exercicio 1 - Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma
+        //estrutura sequencial e uma estrutura de repetição while.
+        int x = 1;
+        while(x <= 11){
+            System.out.println(x + " Hello World!");
+            x = x + 1;
+        }
+
+
+    }
+}
