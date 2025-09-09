2a60958a3cee0c7b7d4cbbdf4da6746cc28a6067
alexnezumi <alexsilva.viana2006@gmail.com>
2025-09-09 08:29:11 -0300

    exercicio 7

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -88,7 +90,41 @@ public class Main {
      // exercicio 6 - Usando o laço while para pedir ao usuário que tente acertar um número aleatório
         //entre 0 e 10 (incluindo) e mostrar a quantidade de tentativas feitas. Dica: Usar o
         //comando Random numero = new Random();
-        
 
+
+
+
+
+
+
+
+
+     // exercicio 7 - O usuário vai digitar uma nota válida (de 0 até 10). O programa deverá armazenar
+        //estas notas em um acumulador. Usar uma segunda variável para contar quantas notas
+        //foram armazenadas. Ao final, quando o usuário não quiser mais lançar notas, ele pede
+        //para sair e o programa imprime na tela a média das notas
+
+        Scanner grava = new Scanner(System.in);
+        double media, nota = 0, x;
+        int menu = 1;
+        int quant = 0;
+
+        while (menu == 1)
+        {
+            System.out.println("Insira a nota");
+            x = grava.nextDouble();
+            if (x >= 0 && x <= 10) {
+                nota = x + nota;
+                quant++;
+
+                System.out.println("Deseja inserir mais alguma nota ? 1/sim 2/não");
+                menu = grava.nextInt();
+
+            }else {
+                System.out.println("Insira uma nota valida que seja de 0 a 10");
+            }
+        }
+        media = nota/quant;
+        System.out.println("A média das notas digitadas são " + media);
     }
 }
